737dc781f8d43faed66a452c504cc9cd71d5a027
์Natnicha Phriksen <natnicha.phr@ku.th>
2021-02-04 16:54:18 +0700

    add for

--- 6320500557_1_5.c
+++ 6320500557_1_5.c
@@ -3,7 +3,7 @@ int main()
 {
     int N;
     scanf("%d",&N);
-    int i,j,a[N];
+    int i,j,a[N],x=0;
     for(i=1;i<=N;i++)
     {
         scanf("%d",&a[i]);
@@ -13,8 +13,10 @@ int main()
         if(j<a[i])
         {
             j = a[i];
+            x++;
+
         }
     }
-    printf("%d %d",i,j);
+    printf("%d %d",x,j);
     return 0;
 }
